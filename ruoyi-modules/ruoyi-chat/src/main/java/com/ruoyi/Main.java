42ddb1fd0519e260d839c648a537611cc73ebe87
liangsy <chenqiang@163.com>
2024-07-19 17:43:09 +0800

    增加chat微服务

---
+++ ruoyi-modules/ruoyi-chat/src/main/java/com/ruoyi/Main.java
@@ -0,0 +1,11 @@
+package com.ruoyi;
+
+/*
+ *@Author:cq
+ *@Date:2024/7/19 15:33
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ ruoyi-modules/ruoyi-chat/src/main/java/com/ruoyi/chat/RuoYiChatApplication.java
@@ -0,0 +1,34 @@
+package com.ruoyi.chat;
+
+import com.ruoyi.common.security.annotation.EnableCustomConfig;
+import com.ruoyi.common.security.annotation.EnableRyFeignClients;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author lsy
+ * @description 聊天应用启动类
+ * @date 2024/7/19
+ */
+@EnableAsync
+@EnableCustomConfig
+@EnableRyFeignClients
+@SpringBootApplication
+public class RuoYiChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuoYiChatApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  聊天模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+
+}
